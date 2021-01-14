532f48bbaf943485534722638b6781714c87c508
dgizer <maxyartur@ymail.com>
2021-01-14 16:03:49 +0100

    add ManyToMany relation for Recepie-Category; add repositories

--- src/main/java/com/springframework/domain/Recipe.java
+++ src/main/java/com/springframework/domain/Recipe.java
@@ -1,5 +1,7 @@
 package com.springframework.domain;
 
+import com.springframework.enums.Difficulty;
+
 import javax.persistence.*;
 import java.util.Set;
 
@@ -16,8 +18,10 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    //todo add
-    //private Difficulty difficulty;
+
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
+
     @Lob
     private Byte[] image;
 
@@ -26,6 +30,12 @@ public class Recipe {
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredientSet;
+
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+            joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
 
     public Long getId() {
         return id;
@@ -91,6 +101,14 @@ public class Recipe {
         this.directions = directions;
     }
 
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
     public Byte[] getImage() {
         return image;
     }
@@ -115,4 +133,11 @@ public class Recipe {
         this.ingredientSet = ingredientSet;
     }
 
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
+    }
 }
