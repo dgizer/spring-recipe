42496c35a30667acb8e3c669cd957151cd2a51a1
dgizer <maxyartur@ymail.com>
2021-02-12 22:08:51 +0100

    Add NumberFormatException Handler for bad (id) requests

--- src/test/java/com/springframework/controllers/RecipeControllerTest.java
+++ src/test/java/com/springframework/controllers/RecipeControllerTest.java
@@ -52,14 +52,19 @@ class RecipeControllerTest {
 
     @Test
     void testGetRecipeNotFound() throws Exception {
-        Recipe recipe = new Recipe();
-        recipe.setId(1L);
 
         when(recipeService.findById(anyLong())).thenThrow(NotFoundException.class);
 
         mockMvc.perform(get("/recipe/1/show"))
                 .andExpect(status().isNotFound())
                 .andExpect(view().name("404error"));
+    }
+
+    @Test
+    void testGetRecipeNumberFormatWrong() throws Exception {
+        mockMvc.perform(get("/recipe/asd/show"))
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name("400error"));
     }
 
     @Test
