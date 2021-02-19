beb025304cee7e53a905c2e8980ff0709848e754
dgizer <maxyartur@ymail.com>
2021-02-19 02:03:32 +0100

    added mysql db and setup connection

--- src/main/java/com/springframework/domain/Category.java
+++ src/main/java/com/springframework/domain/Category.java
@@ -1,7 +1,9 @@
 package com.springframework.domain;
 
+import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.Set;
@@ -9,7 +11,15 @@ import java.util.Set;
 @Data
 @EqualsAndHashCode(exclude = {"recipes"})
 @Entity
+@NoArgsConstructor
 public class Category {
+
+    @Builder
+    public Category(Long id, String description, Set<Recipe> recipes) {
+        this.id = id;
+        this.description = description;
+        this.recipes = recipes;
+    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/springframework/domain/UnitOfMeasure.java
+++ src/main/java/com/springframework/domain/UnitOfMeasure.java
@@ -1,6 +1,8 @@
 package com.springframework.domain;
 
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,7 +11,14 @@ import javax.persistence.Id;
 
 @Data
 @Entity
+@NoArgsConstructor
 public class UnitOfMeasure {
+
+    @Builder
+    public UnitOfMeasure(Long id, String description) {
+        this.id = id;
+        this.description = description;
+    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
